5bb03ed34b92f3f63b6c9305aa2ebee3f049c76b
ManelNavola <ManelNavola@users.noreply.github.com>
2021-03-26 00:53:33 +0100

    Fix16

--- data/user.py
+++ data/user.py
@@ -2,18 +2,14 @@ from utils import utils
 
 class User:
     def __init__(self, user_id, data):
-        print('create')
         self.id = user_id
-        if row:
-            print('there is row')
+        if data:
             self.data = data
         else:
-            print('there is no row')
             self.data = {
                 'money': 10,
                 'money_time': utils.now()
             }
-            print('aight done')
 
     def get_money():
         return self.data['money'] + (self.data['money_time'] - utils.now()) // 60
--- data/user_management.py
+++ data/user_management.py
@@ -15,23 +15,17 @@ def get(user_id):
         
 
 def load(user_id):
-    print('Try loading')
     cur = database.cursor()
     cur.execute(f"""SELECT * from users WHERE id = {user_id}""")
     data = cur.fetchone()
-    print('Creating user')
     print(data)
     user = User(user_id, data)
-    print('done')
     if not data:
-        print('call save')
         save(user, True)
-    print('cleanup')
     try_cleanup()
     return user
 
 def save(user, first_time=False):
-    print('cur')
     cur = database.cursor()
     if first_time:
         print('Try saving (first time)')
