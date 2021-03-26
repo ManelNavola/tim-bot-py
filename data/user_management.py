83053f3cdec4d846ed6a653ffad6c05db949b10c
ManelNavola <ManelNavola@users.noreply.github.com>
2021-03-26 01:04:20 +0100

    Fix19

--- data/user_management.py
+++ data/user_management.py
@@ -31,11 +31,13 @@ def save(user, first_time=False):
         a, b = database.parse_insert(user.data, {'id': user.id})
         print(a, b)
         cur.execute(f"""INSERT INTO users {a} VALUES {b}""")
+        cur.commit()
     else:
         print('Try saving')
         to_save = database.parse_set(user.data)
         print(to_save)
         cur.execute(f"""UPDATE users SET {to_save} WHERE id = {user_id}""")
+        cur.commit()
 
 def try_cleanup():
     pass
